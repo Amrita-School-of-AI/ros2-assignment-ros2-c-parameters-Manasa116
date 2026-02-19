5ad5b4556855c93276be1dd3167c3212c6155c37
Manasa Chittamanasareddy <chittamanasareddy@gmail.com>
2026-02-19 11:24:38 +0530

    Completed ROS2 Parameters assignment

---
+++ src/param_node.cpp
@@ -0,0 +1,45 @@
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+class ParamNode : public rclcpp::Node
+{
+public:
+    ParamNode() : Node("param_node")
+    {
+      this->declare_parameter<std::string>("robot_name", "ROS2Bot");
+      this->declare_parameter<double>("max_speed", 1.5);
+      this->declare_parameter<bool>("enabled", true);
+
+      timer_ = this->create_wall_timer(
+           2000ms,
+           std::bind(&ParamNode::timer_callback, this));
+      }
+
+private:
+    void timer_callback()
+    {
+        std::string robot_name;
+	double max_speed;
+	bool enabled;
+
+        this->get_parameter("robot_name", robot_name);
+	this->get_parameter("max_speed", max_speed);
+	this->get_parameter("enabled", enabled);
+
+	RCLCPP_INFO(this->get_logger(),
+         "Robot: %s, Max Speed: %.6f, Enabled: %s",
+                    robot_name.c_str(),
+                    max_speed,
+                    enabled ? "true" : "false");
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ParamNode>());
+    rclcpp::shutdown();
+    return 0;
+}	 
